56a8de9414554409f62129ef7a36448e36d496ce
paulo <pbrsj1385@gmail.com>
2023-01-16 22:34:19 -0300

    implementa classe cliente geladao application, teste web ok!

--- src/main/java/br/com/geladaosabordoverao/clientegeladao/ClienteGeladaoApplication.java
+++ src/main/java/br/com/geladaosabordoverao/clientegeladao/ClienteGeladaoApplication.java
@@ -2,12 +2,22 @@ package br.com.geladaosabordoverao.clientegeladao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RequestMapping("/")
+@RestController
 public class ClienteGeladaoApplication {
+	@GetMapping
+	public String getHomeTeste(){
+		return "Teste Geladao";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteGeladaoApplication.class, args);
 	}
 
 }
+
