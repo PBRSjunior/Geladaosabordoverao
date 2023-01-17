e0b1b0e783affb5a5c12db7486a0f991f2b90ec1
paulo <pbrsj1385@gmail.com>
2023-01-16 21:43:30 -0300

    inicia projeto

---
+++ src/main/java/br/com/geladaosabordoverao/clientegeladao/ClienteGeladaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.geladaosabordoverao.clientegeladao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteGeladaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteGeladaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/geladaosabordoverao/clientegeladao/ClienteGeladaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geladaosabordoverao.clientegeladao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteGeladaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
